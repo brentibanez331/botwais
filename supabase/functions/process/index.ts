b5337d0a3ce95799c399cf7ea4c1cbb4aeae4f2b
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2025-04-01 18:24:58 +0800

    feat: add embedding edge function and db triggers

--- supabase/functions/process/index.ts
+++ supabase/functions/process/index.ts
@@ -1,32 +1,106 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { createClient } from "@supabase/supabase-js";
+import { processMarkdown } from "../_lib/markdown-parser.ts";
 
-// Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { NextRequest } from "next/server"
+const supabaseUrl = Deno.env.get("SUPABASE_URL");
+const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
 
-console.log("Hello from Functions!")
-
-Deno.serve(async (req: NextRequest) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+Deno.serve(async (req) => {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return new Response(
+      JSON.stringify({
+        error: "Missing environment variables.",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  const authorization = req.headers.get("Authorization");
 
-/* To invoke locally:
+  if (!authorization) {
+    return new Response(
+      JSON.stringify({ error: `No authorization header passed` }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        authorization,
+      },
+    },
+    auth: {
+      persistSession: false,
+    },
+  });
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/process' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-*/
+  const { document_id } = await req.json();
+
+  const { data: document } = await supabase
+    .from("documents_with_storage_path")
+    .select()
+    .eq("id", document_id)
+    .single();
+
+  if (!document?.storage_object_path) {
+    return new Response(
+      JSON.stringify({ error: "Failed to find uploaded document" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  const { data: file } = await supabase.storage
+    .from("files")
+    .download(document.storage_object_path);
+
+  if (!file) {
+    return new Response(
+      JSON.stringify({ error: "Failed to download storage object" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  const fileContents = await file.text();
+
+
+  const processedMd = processMarkdown(fileContents);
+
+  const { error } = await supabase.from("document_sections").insert(
+    processedMd.sections.map(({ content }) => ({
+      document_id,
+      content,
+    }))
+  );
+
+  if (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Failed to save document sections" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  console.log(
+    `Saved ${processedMd.sections.length} sections for file '${document.name}'`
+  );
+
+  return new Response(null, {
+    status: 204,
+    headers: { 'Content-Type': 'application/json' },
+  });
+});
